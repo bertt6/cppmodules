56654ccf2cbecb72fefdf35a18557f31892152f4
Berat Samli <bsamli@k1m31s04.42istanbul.com.tr>
2023-07-17 18:42:30 +0300

    [LAST] CPP01 ex04 updating

--- CPP01/ex04/main.cpp
+++ CPP01/ex04/main.cpp
@@ -2,8 +2,12 @@
 #include <fstream>
 
 int main(int ac, char **av) {
+    (void)ac;
+    (void)av;
+    std::string text = "asdasdasd";
     std::fstream myfile;
     myfile.open("dssadasd.txt");
-    myfile << "selams";
-    myfile.close();
+    myfile << text;
+    getline(myfile, text);
+    std::cout << text;
 }
